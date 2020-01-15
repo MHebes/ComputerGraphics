d1f66461b481c19e2c22097e3543ce0c82b9ead5
Michael Hebert <mike.s.hebert@gmail.com>
2020-01-14 19:24:11 -0500

    Implement PPM

--- Assignment0_CPlusPlus/include/PPM.h
+++ Assignment0_CPlusPlus/include/PPM.h
@@ -3,56 +3,85 @@
  *
  *  Class for working with P3 PPM images specifically.
  *
- *  @author your_name_here
+ *  @author Michael Hebert
  *  @bug No known bugs.
  */
 #ifndef PPM_H
 #define PPM_H
 
+#include <iostream>
 #include <string>
+
+static const char* PPM_MAGIC = "P3";
+static const size_t PPM_MAX = 255;
+static const int PPM_PIXELS_PER_LINE = 8;
+static const unsigned char PPM_DARKEN_AMT = 50;
 
 class PPM {
 public:
-
     // Constructor loads a filename with the .ppm extension
     PPM(std::string fileName);
-    
+
     // Destructor clears any memory that has been allocated
     ~PPM();
-    
+
+    // overload << operator to print to streams
+    inline friend std::ostream& operator<<(std::ostream& os, const PPM& ppm)
+    {
+        os << "P3" << std::endl;
+        os << ppm.getWidth() << ' ' << ppm.getHeight() << std::endl;
+        os << PPM_MAX << std::endl;
+
+        for (int y = 0; y < ppm.getHeight(); y++) {
+            for (int x = 0; x < ppm.getWidth(); x++) {
+                unsigned char* pixel = ppm.getPixel(x, y);
+                os << +pixel[0] << " " << +pixel[1] << " " << +pixel[2];
+
+                // add newline if at max, or space between them
+                if (x % PPM_PIXELS_PER_LINE == PPM_PIXELS_PER_LINE - 1) os << std::endl;
+                else if (x < ppm.getWidth() - 1) os << "  ";
+            }
+            os << std::endl;
+        }
+
+        return os;
+    }
+
     // Saves a PPM Image to a new file.
-    void savePPM(std::string outputFileName);
-    
+    void savePPM(std::string outputFileName) const;
+
     // Darken subtracts 50 from each of the red, green
     // and blue color components of all of the pixels
     // in the PPM. Note that no values may be less than
     // 0 in a ppm.
     void darken();
-    
+
     // Sets a pixel to a specific R,G,B value
-    void setPixel(int x, int y, int R, int G, int B);
-    
+    void setPixel(size_t x, size_t y, unsigned char R, unsigned char G,
+                  unsigned char B);
+
+    unsigned char* getPixel(size_t x, size_t y) const;
+
     // Returns the raw pixel data in an array.
     // You may research what 'inline' does.
-    inline unsigned char* pixelData() { return m_PixelData; }
-    
+    inline unsigned char* pixelData() const { return m_PixelData; }
+
     // Returns image width
-    inline int getWidth() { return m_width; }
-    
+    inline size_t getWidth() const { return m_width; }
+
     // Returns image height
-    inline int getHeight() { return m_height; }
-    
+    inline size_t getHeight() const { return m_height; }
+
     // NOTE:    You may add any helper functions you like in the
     //          private section.
 private:
-    
     // Store the raw pixel data here
-    // Data is R,G,B format
+    // Data is R,G,B format row-major order
     unsigned char* m_PixelData;
-    
+
     // Store width and height of image.
-    int m_width{0};
-    int m_height{0};
+    size_t m_width{0};
+    size_t m_height{0};
 };
 
 #endif
--- Assignment0_CPlusPlus/src/ppm.cpp
+++ Assignment0_CPlusPlus/src/ppm.cpp
@@ -1,19 +1,90 @@
+#include <cassert>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <sstream>
+
 #include "PPM.h"
 
-// Constructor loads a filename with the .ppm extension
-PPM::PPM(std::string fileName)
+std::istream& cmt(std::istream& is)
 {
-    // TODO:    Load and parse a ppm to get its pixel
-    //          data stored properly.
+    is >> std::ws;
+    while (is.peek() == '#') {
+        // advance to EOL or EOF
+        is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        is >> std::ws;  // skip leading whitespace
+    }
+
+    return is;
+}
+
+// scales x from range [0, a] to range [0, b]
+size_t scale(const size_t x, const size_t a, size_t b)
+{
+    return b * ((float)x) / ((float)a);
+}
+
+// Constructor loads a filename with the .ppm extension
+PPM::PPM(std::string fileName) : m_PixelData(nullptr)
+{
+    std::ifstream infile(fileName);
+
+    if (!infile) {
+        std::cout << "Error: could not open input file" << std::endl;
+        exit(1);
+    }
+    
+    // read header
+    std::string magic;
+    infile >> magic; // TODO this won't allow things like P3#comment at the start
+
+    if (magic.compare(PPM_MAGIC) != 0) {
+        std::cout << "Error: not a supported file format" << std::endl;
+        exit(1);
+    }
+
+    // read width, height
+    infile >> cmt >> m_width >> cmt >> m_height;
+
+    m_PixelData = new unsigned char[m_width * m_height * 3];
+
+    // read max value
+    size_t max;
+    infile >> cmt >> max;
+
+    // read data
+    for (int y = 0; y < m_height; y++) {
+        for (int x = 0; x < m_width; x++) {
+            size_t r, g, b;
+            infile >> cmt >> r >> cmt >> g >> cmt >> b;
+            // TODO error handling when no data
+
+            // scale from maximum to PPM_MAX
+            r = scale(r, max, PPM_MAX);
+            g = scale(g, max, PPM_MAX);
+            b = scale(b, max, PPM_MAX);
+            setPixel(x, y, r, g, b);
+        }
+    }
 }
 
 // Destructor clears any memory that has been allocated
-PPM::~PPM() {}
+PPM::~PPM() { delete[] m_PixelData; }
 
 // Saves a PPM Image to a new file.
-void PPM::savePPM(std::string outputFileName)
+void PPM::savePPM(std::string outputFileName) const
 {
-    // TODO: Save a PPM image to disk
+    assert(m_PixelData);  // invariant
+
+    std::ofstream outfile(outputFileName);
+
+    if (!outfile) {
+        std::cout << "Error: could not open output file" << std::endl;
+        exit(1);
+    }
+
+    outfile << *this;
 }
 
 // Darken subtracts 50 from each of the red, green
@@ -22,11 +93,23 @@ void PPM::savePPM(std::string outputFileName)
 // 0 in a ppm.
 void PPM::darken()
 {
-    // TODO: Output a 'filtered' PPM image.
+    for (size_t i = 0; i < m_width * m_height * 3; i++) {
+        m_PixelData[i] =
+            (m_PixelData[i] <= PPM_DARKEN_AMT ? 0 : m_PixelData[i] - 50);
+    }
+}
+
+unsigned char* PPM::getPixel(size_t x, size_t y) const
+{
+    return &m_PixelData[3 * (y * m_width + x % m_width)];
 }
 
 // Sets a pixel to a specific R,G,B value
-void PPM::setPixel(int x, int y, int R, int G, int B)
+void PPM::setPixel(size_t x, size_t y, unsigned char R, unsigned char G,
+                   unsigned char B)
 {
-    // TODO: Implement
+    size_t basei = 3 * (y * m_width + x % m_width);
+    m_PixelData[basei] = R;
+    m_PixelData[basei + 1] = G;
+    m_PixelData[basei + 2] = B;
 }
